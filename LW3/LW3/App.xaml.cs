2bb7e498307dd6b75766e891939e7fe1fd882c77
ShepelM1 <mucola2000@gmail.com>
2024-03-17 16:02:51 +0200

    LW2-3

---
+++ LW3/LW3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LW3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LW3/LW3/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace LW3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public int Fibonacci(int n)
+        {
+            if (n < 2) return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        private void CalculateFibonacci(object sender, EventArgs e)
+        {
+            if (int.TryParse(inputEntry.Text, out int n))
+            {
+                int result = Fibonacci(n);
+                resultLabel.Text = $"Fibonacci({n}) = {result}";
+            }
+            else
+            {
+                resultLabel.Text = "Please enter a valid number.";
+            }
+            SemanticScreenReader.Announce("Hi");
+        }
+    }
+}
+
+
